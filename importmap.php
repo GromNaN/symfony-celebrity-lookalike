11cdd0bb56e2e2e523794ca2a4d470f351ffc1c4
Jérôme Tamarelle <jerome@tamarelle.net>
2025-05-13 16:42:14 +0200

    Add debug packages (#8)

--- importmap.php
+++ importmap.php
@@ -1,32 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
-use Symfony\Component\AssetMapper\ImportMap\ImportMapConfigBuilder;
-
-return static function (ImportMapConfigBuilder $config): void {
-    // Automatically detect controllers in the controllers/ directory
-    $config->autoImportDirectory(__DIR__ . '/assets/controllers');
-
-    // The path is relative to this file
-    $config->autoProvideAssets(['assets/app.js']);
-
-    // If the package is available in a CDN, it can be imported
-    $config->addPackage('stimulus', [
-        'url' => 'https://cdn.jsdelivr.net/npm/@hotwired/stimulus@3.2.2/+esm',
-        'version' => '3.2.2',
-    ]);
-    
-    $config->addPackage('@hotwired/stimulus', [
-        'url' => 'https://cdn.jsdelivr.net/npm/@hotwired/stimulus@3.2.2/+esm',
-        'version' => '3.2.2',
-    ]);
-    
-    $config->addPackage('@symfony/stimulus-bundle', [
-        'url' => 'https://cdn.jsdelivr.net/npm/@symfony/stimulus-bundle@2.24.0/+esm',
-        'version' => '2.24.0',
-    ]);
-};
 /**
  * Returns the importmap for this application.
  *
@@ -38,15 +11,14 @@ return static function (ImportMapConfigBuilder $config): void {
  *
  * The "importmap:require" command can be used to add new entries to this file.
  */
+
+declare(strict_types=1);
+
 return [
     'app' => [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
-    '@hotwired/stimulus' => [
-        'version' => '3.2.2',
-    ],
-    '@symfony/stimulus-bundle' => [
-        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
-    ],
+    '@hotwired/stimulus' => ['version' => '3.2.2'],
+    '@symfony/stimulus-bundle' => ['path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js'],
 ];
