2489d06b3c61a35dd12a771a673f10e45e4d846d
Andreas Braun <andreas.braun@mongodb.com>
2025-05-15 17:03:56 +0200

    Add event listener to fix vector search indexes

---
+++ src/Doctrine/ODM/MongoDB/Listener/VectorSearchIndexListener.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Doctrine\ODM\MongoDB\Listener;
+
+use App\Document\Face;
+use Doctrine\Bundle\MongoDBBundle\Attribute\AsDocumentListener;
+use Doctrine\ODM\MongoDB\Event\LoadClassMetadataEventArgs;
+use Doctrine\ODM\MongoDB\Events;
+
+use function array_map;
+
+#[AsDocumentListener(Events::loadClassMetadata)]
+class VectorSearchIndexListener
+{
+    public function loadClassMetadata(LoadClassMetadataEventArgs $args): void
+    {
+        $classMetadata = $args->getClassMetadata();
+
+        if ($classMetadata->name !== Face::class) {
+            return;
+        }
+
+        // Remove the original search index definition - ODM only supports Atlas Text Search
+        $searchIndexes = array_map(
+            static function (array $index) {
+                if ($index['name'] !== 'faces' || ($index['type'] ?? null) === 'vectorSearch') {
+                    return $index;
+                }
+
+                $index['type'] = 'vectorSearch';
+                $index['definition']['fields'] = $index['definition']['mappings']['fields'];
+                unset($index['definition']['mappings']);
+
+                return $index;
+            },
+            $classMetadata->searchIndexes,
+        );
+
+        // A big no-no: override the search indexes
+        $classMetadata->searchIndexes = $searchIndexes;
+    }
+}
---
+++ tests/Document/FaceTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Document;
+
+use App\Document\Face;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class FaceTest extends KernelTestCase
+{
+    public function testSearchIndex(): void
+    {
+        self::bootKernel();
+
+        $classMetadata = $this->getContainer()
+            ->get('doctrine_mongodb.odm.default_document_manager')
+            ->getClassMetadata(Face::class);
+
+        $this->assertEquals(
+            [
+                [
+                    'name' => 'faces',
+                    'type' => 'vectorSearch',
+                    'definition' => [
+                        'fields' => [
+                            [
+                                'numDimensions' => 1024,
+                                'path' => 'embeddings',
+                                'similarity' => 'euclidean',
+                                'type' => 'vector',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            $classMetadata->searchIndexes,
+        );
+    }
+}
